b1d6fa7b11db1f80f4279f7eb50fb1eee99906f7
aryanpandey <aryan.pandey@outlook.com>
2021-06-30 13:19:17 +0530

    Model Improvements

--- Model/Model_Building.py
+++ Model/Model_Building.py
@@ -11,7 +11,7 @@ import pickle
 import nltk
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split, StratifiedKFold
-from sklearn.metrics import f1_score, mean_squared_error
+from sklearn.metrics import f1_score, mean_squared_error, accuracy_score
 from sklearn.decomposition import PCA
 from imblearn.over_sampling import SMOTE
 #nltk.download('punkt')
@@ -117,11 +117,13 @@ for i in text_columns[:-1]:
 
 #'''
 def bin_score(entry):
-    if entry>4:
+    if entry>4.5:
+        return 4
+    elif entry>4:
         return 3
     elif entry>3:
         return 2
-    elif entry>2:
+    elif entry>1.75:
         return 1
     else:
         return 0
@@ -132,17 +134,17 @@ data['score'] = data['score'].apply(bin_score)
 data.dropna(inplace=True)
 X = data.drop(['score'], axis = 1)
 Y = data['score']
-print(X.columns)
 
-kfold = StratifiedKFold(n_splits = 3, shuffle = True, random_state = 42)
+
+kfold = StratifiedKFold(n_splits = 5, shuffle = True, random_state = 42)
 
 splits = kfold.split(X, Y)
-macro_scores = []
+acc_scores = []
 weighted_scores = []
 
 from xgboost import XGBClassifier, XGBRegressor
 
-for Train, Test in splits:
+for i, (Train, Test) in enumerate(splits):
     X_Train, X_Test, Y_Train, Y_Test = X.iloc[Train], X.iloc[Test], Y.iloc[Train], Y.iloc[Test]
     
     sm = SMOTE(random_state = 42, n_jobs = -1)
@@ -151,13 +153,16 @@ for Train, Test in splits:
     xgb = XGBClassifier(n_estimators = 2000, n_jobs = -1, learning_rate = 0.1, reg_lambda = 0.1, objective = 'multiclassova')
     xgb.fit(X_Train, Y_Train, early_stopping_rounds = 100, eval_set = [(X_Train, Y_Train), (X_Test, Y_Test)], eval_metric = 'mlogloss',  verbose = False)
     
+    with open('../Model_Data/model_'+str(i)+'.pkl', 'wb') as f:
+        pickle.dump(xgb, f)
+    
     pred = xgb.predict(X_Test)
     
-    macro_scores.append(f1_score(Y_Test, pred, average = 'macro'))
+    acc_scores.append(accuracy_score(Y_Test, pred))
     weighted_scores.append(f1_score(Y_Test, pred, average = 'weighted'))
     
 #print("RMSE:", mean_squared_error(Y_CV, pred, squared = False))
 #'''
 print("Weighted F1:", sum(weighted_scores)/len(weighted_scores))
-print("Macro F1:", sum(macro_scores)/len(macro_scores))
+print("Accuracy:", sum(acc_scores)/len(acc_scores))
 #'''
